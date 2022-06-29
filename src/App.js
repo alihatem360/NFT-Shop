bbab094145b51f4d68f8686bf230441e48f8d8bd
Ali Hatem <68661572+alihatem360@users.noreply.github.com>
2022-06-29 10:57:56 +0200

    update root page

--- src/App.js
+++ src/App.js
@@ -12,46 +12,7 @@ function App() {
   const [Filtter, setFiltter] = useState("");
   const [isAdmin, setAdmin] = useState(false);
   const [cart, setCart] = useState([]);
-  const [cartItem, setCartItem] = useState([
-    {
-      "id": 1,
-      "name": "Les Immort",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/95/eb/12/95eb12b6aff17089523caed5f1a9c1a8.jpg"
-    },
-    {
-      "id": 2,
-      "name": "Les Immort",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/fa/ec/8f/faec8f78a0cdf4360a03fd4284b0d712.jpg"
-    },
-    {
-      "id": 3,
-      "name": "Les Immort",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/44/25/47/44254767baf386eb280ea283bfb6c04f.jpg"
-    },
-    {
-      "id": 4,
-      "name": "Les Immort",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/62/32/66/62326694ce4a7b0a59bb5601b9bdb79a.jpg"
-    },
-  
-    {
-      "id": 5,
-      "name": "sdfsdf",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/62/32/66/62326694ce4a7b0a59bb5601b9bdb79a.jpg"
-    },
-  
-    {
-      "id": 6,
-      "name": "sdfsdf",
-      "price": 123,
-      "image": "https://i.pinimg.com/750x/62/32/66/62326694ce4a7b0a59bb5601b9bdb79a.jpg"
-    }
-  ]);
+  const [cartItem, setCartItem] = useState([]);
 
   useEffect(() => {
     document.title = `Hi ${TilteName}`;
@@ -76,11 +37,11 @@ function App() {
     setAdmin(!isAdmin);
   };
 
-  // useEffect(() => {
-  //   fetch("https://alihatem360.github.io/Database_API/NFT_API.json")
-  //     .then((response) => response.json())
-  //     .then(setCartItem);
-  // }, []);
+  useEffect(() => {
+    fetch("https://alihatem360.github.io/Database_API/NFT_API.json")
+      .then((response) => response.json())
+      .then(setCartItem);
+  }, []);
 
   const handelDelete = (elementid) => {
     console.log(elementid);
